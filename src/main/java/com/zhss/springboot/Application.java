40ae61f84f71429a4a18b2ab2b7b8d9ca2d251dd
liugf <863065649@qq.com>
2020-08-08 01:25:55 +0800

    Application add

--- src/main/java/com/zhss/springboot/Application.java
+++ src/main/java/com/zhss/springboot/Application.java
@@ -91,9 +91,9 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
-//        SpringApplication app = new SpringApplication(Application.class);
-//        app.addListeners(new MyApplicationStartedEventListener());
-//        app.run(args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.addListeners(new MyApplicationStartedEventListener());
+        app.run(args);
 
     }
 
